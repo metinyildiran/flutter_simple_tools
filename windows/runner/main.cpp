21b22fd099dc2efbd4a844df0d421a6b9dc97285
Metin Yıldıran <metin.1464@gmail.com>
2021-12-11 21:29:00 +0300

    Changed window size and location.

--- windows/runner/main.cpp
+++ windows/runner/main.cpp
@@ -25,9 +25,9 @@ int APIENTRY wWinMain(_In_ HINSTANCE instance, _In_opt_ HINSTANCE prev,
   project.set_dart_entrypoint_arguments(std::move(command_line_arguments));
 
   FlutterWindow window(project);
-  Win32Window::Point origin(10, 10);
-  Win32Window::Size size(1280, 720);
-  if (!window.CreateAndShow(L"simple_tools", origin, size)) {
+  Win32Window::Point origin(1250, 600);
+  Win32Window::Size size(270, 200);
+  if (!window.CreateAndShow(L"Simple Tools", origin, size)) {
     return EXIT_FAILURE;
   }
   window.SetQuitOnClose(true);
